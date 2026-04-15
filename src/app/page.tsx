a86483abf020a1ae0d3fe048072c7d6e1004e9d0
Fajrul Santoso <fajrulsantoso10@gmail.com>
2026-04-15 16:06:38 +0700

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,5 @@
-'use client';
-
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
+import { redirect } from 'next/navigation';
 
 export default function Page() {
-  const router = useRouter();
-
-  useEffect(() => {
-    const userId = localStorage.getItem('userId');
-
-    if (!userId) {
-      router.push('/auth/sign-in');
-    } else {
-      router.push('/dashboard/overview');
-    }
-  }, [router]);
-
-  return null;
+  redirect('/dashboard');
 }
